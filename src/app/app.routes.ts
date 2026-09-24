4a8f4255ce95591eae23bd6b3e9c94e615cdc331
trinobank-rodrigoarruda <173008722+Rarruda24@users.noreply.github.com>
2026-09-24 16:59:46 -0300

    feat: estrutura o layout principal do ServiceHub

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard').then(
+            (m) => m.Dashboard
+          ),
+      },
+      {
+        path: 'clients',
+        loadComponent: () =>
+          import('./pages/clients/clients').then(
+            (m) => m.Clients
+          ),
+      },
+      {
+        path: 'tickets',
+        loadComponent: () =>
+          import('./pages/tickets/tickets').then(
+            (m) => m.Tickets
+          ),
+      },
+    ],
+  },
+];
--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './sidebar/sidebar';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Sidebar,
+    Header
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
