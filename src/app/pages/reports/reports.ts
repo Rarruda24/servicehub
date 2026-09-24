53c47fc93f9cb97f121440d744a0a2ba1084061d
trinobank-rodrigoarruda <173008722+Rarruda24@users.noreply.github.com>
2026-09-24 18:14:09 -0300

    feat: cria pagina de relatorios do ServiceHub

---
+++ src/app/pages/reports/reports.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-reports',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './reports.html',
+  styleUrl: './reports.css',
+})
+export class Reports {}
