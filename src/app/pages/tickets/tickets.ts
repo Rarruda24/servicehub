4c5cb39e831b5451e1b1b215f3f67e2a303e2bbb
trinobank-rodrigoarruda <173008722+Rarruda24@users.noreply.github.com>
2026-09-24 17:18:07 -0300

    feat: cria pagina de tickets do ServiceHub

--- src/app/pages/tickets/tickets.ts
+++ src/app/pages/tickets/tickets.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule],
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
 })
