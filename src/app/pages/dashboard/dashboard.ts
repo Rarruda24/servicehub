e04ebbbcebc8322f4f24439b7dd9710c2b6a92be
trinobank-rodrigoarruda <173008722+Rarruda24@users.noreply.github.com>
2026-09-24 17:11:28 -0300

    feat: cria dashboard do ServiceHub

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
